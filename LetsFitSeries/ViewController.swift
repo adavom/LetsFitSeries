94473d89b54366d019d007b2550ba9babc818f90
Alejandro Davó <alejandro.davo@gmail.com>
2018-09-24 08:59:20 -0700

    version inicial

---
+++ LetsFitSeries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LetsFitSeries
+//
+//  Created by user145804 on 9/24/18.
+//  Copyright © 2018 Alejandro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
